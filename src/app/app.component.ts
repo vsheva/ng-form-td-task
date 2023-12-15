025fe74c3410c27b9648869d907bf2e8b0437215
vsheva <valera.sheva@gmail.com>
2023-12-15 00:51:38 -0500

    patch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,39 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signup:NgForm  //!!passing "f" as string
+  @ViewChild('f') signup: NgForm  //!!passing "f" as string
 
-  defaultQ="teacher";
-  answer:"";
+  defaultQ = "teacher";
+  answer: "";
+  genders = ["Male", "Female"]
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    /* this.signup.setValue({
+       userData:{
+         username:suggestedName,
+         email:''
+       },
+       secret:"pet",
+       questionAnswer:"",
+       gender:'male'
+     })*/
+
+    this.signup.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+
   }
 
- /* onSubmit(form:NgForm) {
-    console.log(form)
-  }*/
+  /* onSubmit(form:NgForm) {
+     console.log(form)
+   }*/
 
   onSubmit() {
-  console.log(this.signup)
+    console.log(this.signup)
   }
 
 }
